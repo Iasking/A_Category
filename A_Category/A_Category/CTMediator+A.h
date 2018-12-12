4beadeaedaf98fba57f26257b24b46117bef0c73
1012948435@qq.com <jinpeng@chezhibao.com>
2018-12-12 20:11:42 +0800

    add spec

--- A_Category/A_Category/CTMediator+A.h
+++ A_Category/A_Category/CTMediator+A.h
@@ -6,8 +6,9 @@
 //  Copyright © 2018年 Parker. All rights reserved.
 //
 
-
+#import <CTMediator/CTMediator.h>
+#import <UIKit/UIKit.h>
 
 @interface CTMediator (A)
-
+- (UIViewController *)A_aViewController;
 @end

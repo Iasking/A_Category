cac61d584ac3002a83cb0aab8cfaa1613bcf5df8
1012948435@qq.com <jinpeng@chezhibao.com>
2018-12-12 20:08:46 +0800

    first commit

---
+++ A_Category/A_Category/CTMediator+A.h
@@ -0,0 +1,13 @@
+//
+//  CTMediator+A.h
+//  A_Category
+//
+//  Created by Parker on 2018/12/4.
+//  Copyright © 2018年 Parker. All rights reserved.
+//
+
+
+
+@interface CTMediator (A)
+
+@end
---
+++ A_Category/A_Category/CTMediator+A.m
@@ -0,0 +1,13 @@
+//
+//  CTMediator+A.m
+//  A_Category
+//
+//  Created by Parker on 2018/12/4.
+//  Copyright © 2018年 Parker. All rights reserved.
+//
+
+#import "CTMediator+A.h"
+
+@implementation CTMediator (A)
+
+@end
